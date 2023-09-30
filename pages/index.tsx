047454eca6fa552a060ba016af34528c7f6a7426
Hernán Astudillo <hernan.astudillo.r@gmail.com>
2023-09-30 20:09:18 -0300

    feat(): bar chart with react 18

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-
-import {Header} from '../components/Header'
-
+import { Header } from "../components/Header";
+import BarChart from "../components/BarChart";
+import Speedometer from "../components/Speedometer";
 
 export default function Home() {
+
   return (
     <main>
-      <Header/>
+      <Header />
+      <div className="p-4 grid md:grid-cols-4 grid-cols-1 gap-4">
+        <BarChart />
+        <BarChart />
+        <Speedometer/>
+      </div>
     </main>
-  )
+  );
 }
