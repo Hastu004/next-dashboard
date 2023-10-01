9b25dc9212b41fbd24dd24fbf3b90bdbd7a744f6
Hernán Astudillo <hernan.astudillo.r@gmail.com>
2023-10-01 16:45:36 -0300

    fix() format code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,34 @@
-import SpeedoMeterChart from '../components/SpeedoMeterChart'
+import SpeedoMeterChart from "../components/SpeedoMeterChart";
 import { Header } from "../components/Header";
-import { Key } from 'react';
+import { Key } from "react";
 
 function ChartList({ charts }: any) {
   return (
     <main>
       <Header />
       <div className="p-4 grid md:grid-cols-4 grid-cols-1 gap-4">
-      {charts.map((chart: { id: Key | null | undefined; })=> {
-        return (
-          <div key={chart.id}>
-            <SpeedoMeterChart chart={chart} />
-          </div>
-        )
-      })}
+        {charts.map((chart: { id: Key | null | undefined }) => {
+          return (
+            <div key={chart.id}>
+              <SpeedoMeterChart chart={chart} />
+            </div>
+          );
+        })}
       </div>
     </main>
-  )
+  );
 }
 
-export default ChartList
+export default ChartList;
 
 export async function getStaticProps() {
-    console.log("SERVER SIDE RENDERING");
-  const response = await fetch('http://localhost:4000/charts')
-  const data = await response.json()
-  //   console.log(data)
+  console.log("SERVER SIDE RENDERING");
+  const response = await fetch("http://localhost:4000/charts");
+  const data = await response.json();
 
   return {
     props: {
-      charts: data
-    }
-  }
+      charts: data,
+    },
+  };
 }
